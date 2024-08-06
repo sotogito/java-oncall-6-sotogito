8e6791fa19d2ff20ec0953d61ec803c0ac111cc4
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-06 17:45:53 +0900

    feat : 출력물 Fromatter 작성

---
+++ src/main/java/oncall/util/OnCallListFormatter.java
@@ -0,0 +1,16 @@
+package oncall.util;
+
+
+public class OnCallListFormatter {
+    private final static String ON_CALL_FORMAT = "%d월 %d일 %s %s";
+    private final static String ON_CALL_HOLIDAY_FORMAT = "%d월 %d일 %s(휴일) %s";
+
+    /*
+    private static String format(Day day){
+        if(day.isHolliday){
+            ON_CALL_HOLIDAY_FORMAT
+        }
+    }
+
+     */
+}
