cb64cd21d4267bf4aa40661a5af9387327d795b8
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-06 17:52:08 +0900

    feat : staff 객체 작성

---
+++ src/main/java/oncall/domain/member/Staff.java
@@ -0,0 +1,13 @@
+package oncall.domain.member;
+
+public class Staff {
+    private final String name;
+
+    public Staff(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
