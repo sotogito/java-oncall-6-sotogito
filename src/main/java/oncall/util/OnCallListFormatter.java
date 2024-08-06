987e309223c4603e7b2f10f9d7ab561c309206ee
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-06 20:50:48 +0900

    feat : (휴일)인키 판단후 출력물 파싱 클래스 작성

--- src/main/java/oncall/util/OnCallListFormatter.java
+++ src/main/java/oncall/util/OnCallListFormatter.java
@@ -1,16 +1,24 @@
 package oncall.util;
 
 
+import oncall.domain.schedule.Day;
+
 public class OnCallListFormatter {
     private final static String ON_CALL_FORMAT = "%d월 %d일 %s %s";
     private final static String ON_CALL_HOLIDAY_FORMAT = "%d월 %d일 %s(휴일) %s";
 
-    /*
-    private static String format(Day day){
-        if(day.isHolliday){
-            ON_CALL_HOLIDAY_FORMAT
+
+    public static String format(Day day){
+        int monthPrintout = day.getMonth();
+        int dayPrintout = day.getDay();
+        String dayOfWeekPrintout = day.getDayOfWeek();
+        String staffName = day.getStaff().getName();
+
+        if(day.isAddHolidayMark()){
+            return String.format(ON_CALL_HOLIDAY_FORMAT,monthPrintout,dayPrintout,dayOfWeekPrintout,staffName);
         }
+        return String.format(ON_CALL_FORMAT,monthPrintout,dayPrintout,dayOfWeekPrintout,staffName);
     }
 
-     */
+
 }
