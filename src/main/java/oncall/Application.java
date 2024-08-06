434a54e7ecb881df1af9c1568e0461f75458ee45
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-06 15:10:09 +0900

    파일 초기화

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,11 +1,8 @@
 package oncall;
 
-import oncall.controller.OnCall;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        OnCall onCall = new OnCall();
-        onCall.run();
+
     }
 }
