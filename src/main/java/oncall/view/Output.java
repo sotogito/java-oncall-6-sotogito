3c0e7c8635771e7f27f57bfb76f839bcc0056aa2
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-06 20:49:17 +0900

    feat : 에러, 온콜 출력물 출력 output 작성

---
+++ src/main/java/oncall/view/Output.java
@@ -0,0 +1,12 @@
+package oncall.view;
+
+public class Output {
+    public static void printError(String error) {
+        System.out.print("[ERROR] " + error);
+    }
+
+    public static void printOnCallList(String onCallList) {
+        System.out.print(onCallList);
+    }
+
+}
